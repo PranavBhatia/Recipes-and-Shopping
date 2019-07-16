445bd0b0c202656de90266b901b91a7a38894305
PranavBhatia <bhatiapranav1996@gmail.com>
2019-07-16 18:11:57 -0400

    create and add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecepieListComponent } from './recepie-book/recepie-list/recepie-list.component';
+import { RecepieItemComponent } from './recepie-book/recepie-list/recepie-item/recepie-item.component';
+import { RecepieDetailComponent } from './recepie-book/recepie-detail/recepie-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepieBookComponent } from './recepie-book/recepie-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecepieListComponent,
+    RecepieItemComponent,
+    RecepieDetailComponent,
+    HeaderComponent,
+    RecepieBookComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recepie-book/recepie-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie-book',
+  templateUrl: './recepie-book.component.html',
+  styleUrls: ['./recepie-book.component.css']
+})
+export class RecepieBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
