2965cf083aaebfcba0fca160a5605d5038666f78
PranavBhatia <bhatiapranav1996@gmail.com>
2019-07-16 18:56:39 -0400

    formatting code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecepieListComponent } from './recepie-book/recepie-list/recepie-list.component';
-import { RecepieItemComponent } from './recepie-book/recepie-list/recepie-item/recepie-item.component';
-import { RecepieDetailComponent } from './recepie-book/recepie-detail/recepie-detail.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
 import { HeaderComponent } from './header/header.component';
-import { RecepieBookComponent } from './recepie-book/recepie-book.component';
+import { RecepieBookComponent } from './recepies/recepie-book.component';
 
 @NgModule({
   declarations: [
