a5907cb81e6a2c57bc85d570655f63b41139cacd
PranavBhatia <bhatiapranav1996@gmail.com>
2019-07-17 14:37:42 -0400

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
-  recepies = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('Test Recipe', 'A dummy kind of description', 'https://www.bbcgoodfood.com/sites/default/files/categories/categories-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    // tslint:disable-next-line: max-line-length
+    new Recipe('Test Recipe', 'A dummy kind of description', 'https://www.bbcgoodfood.com/sites/default/files/categories/categories-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
