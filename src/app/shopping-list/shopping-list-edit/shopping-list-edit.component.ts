323b745be8d0ce3f2109e2b236859e31e7354e7a
PranavBhatia <bhatiapranav1996@gmail.com>
2019-10-15 11:01:54 -0400

    Revert "TD: Adding the Shopping List Form"
    This reverts commit 37249d2de732802cefc53b487704e955702b6f01.

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
-import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -9,16 +8,19 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInput: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInput: ElementRef;
+
   constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit() {
   }
 
-  onAddIngredient(form: NgForm) {
-    const value = form.value;
-    const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+  onAddIngredient() {
+    const ingName = this.nameInput.nativeElement.value;
+    const ingAmount = this.amountInput.nativeElement.value;
+    this.shoppingListService.addIngredient(new Ingredient(ingName, ingAmount));
   }
 
 }
