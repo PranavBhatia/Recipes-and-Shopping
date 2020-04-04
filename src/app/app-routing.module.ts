bfe2807ad720afb7db28f26e5bb823f87d95b930
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-04 19:21:34 -0400

    Rename recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {RecipeBookComponent} from './recipes/recipes.component';
+import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
@@ -13,7 +13,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {
     path: 'recipes',
-    component: RecipeBookComponent,
+    component: RecipesComponent,
     canActivate: [AuthGuard],
     children: [
       {
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RecipeListComponent} from './recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
-import {RecipeBookComponent} from './recipes.component';
+import {RecipesComponent} from './recipes.component';
 import {RecipeStartComponent} from './recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
 import {RouterModule} from '@angular/router';
@@ -14,7 +14,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeBookComponent,
+    RecipesComponent,
     RecipeStartComponent,
     RecipeEditComponent
   ],
@@ -27,7 +27,7 @@ import {ReactiveFormsModule} from '@angular/forms';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeBookComponent,
+    RecipesComponent,
     RecipeStartComponent,
     RecipeEditComponent
   ]
