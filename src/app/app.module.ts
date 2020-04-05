5b1daff8112f07cfa3c8a4889368c55705700ef8
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-04 20:08:35 -0400

    The ShoppingList Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,11 @@ import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {AlertComponent} from './shared/alert/alert.component';
 import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
 import {RecipesModule} from './recipes/recipes.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
     HeaderComponent,
     DropdownDirective,
     AuthComponent,
@@ -35,7 +34,8 @@ import {RecipesModule} from './recipes/recipes.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [
     ShoppingListService,
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingListEditComponent} from './shopping-list-edit/shopping-list-edit.component';
+import {ShoppingListRoutingModule} from './shopping-list-routing.module';
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ShoppingListRoutingModule
+  ]
+})
+export class ShoppingListModule {
+
+}
