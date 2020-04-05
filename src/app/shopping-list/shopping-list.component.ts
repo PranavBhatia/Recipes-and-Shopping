ab147d60376956b9adcf98141e63f0c9a5e17a9a
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-04 22:10:40 -0400

    Loading Services Differently

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 import {Subscription} from 'rxjs';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -12,10 +13,14 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private subscription: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) {
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private loggingService: LoggingService
+  ) {
   }
 
   ngOnInit() {
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
     this.ingredients = this.shoppingListService.getIngredients();
     this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
