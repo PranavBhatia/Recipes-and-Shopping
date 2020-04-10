41ba87bec00a188642186a6fe848bbb266b99ac7
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-10 18:22:22 -0400

    Dispatching Actions + Remove Logging service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth/auth.service';
-import {LoggingService} from './logging.service';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +9,11 @@ import {LoggingService} from './logging.service';
 export class AppComponent implements OnInit {
   constructor(
     private authService: AuthService,
-    private loggingService: LoggingService
   ) {
   }
 
   ngOnInit() {
     this.authService.autoLogin();
-    this.loggingService.printLog('Hello from AppComponent ngOnInit!');
   }
 
 }
