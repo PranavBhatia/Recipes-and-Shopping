cfeed18e05d0f8821f1cbcc0c01d8af98284c55b
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-11 18:11:25 -0400

    Setting Up Auth Reducer & Actions

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,17 @@
+import {Action} from '@ngrx/store';
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export class Login implements Action {
+  readonly type: string = LOGIN;
+
+  constructor(public payload: { email: string, userId: string, token: string, expiresDate: Date }) {
+  }
+}
+
+export class Logout implements Action {
+  readonly type: string = LOGOUT;
+}
+
+export type AuthActions = Login | Logout;
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import {User} from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
@@ -8,6 +9,25 @@ const initialState: State = {
   user: null
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        (action as AuthActions.Login).payload.email,
+        (action as AuthActions.Login).payload.userId,
+        (action as AuthActions.Login).payload.token,
+        (action as AuthActions.Login).payload.expiresDate
+      );
+      return {
+        ...state,
+        user
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
 }
