0571669208f69cd6259a3803f0673dced75fd61a
PranavBhatia <bhatiapranav1996@gmail.com>
2020-04-12 20:13:59 -0400

    Finishing the Login Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,11 @@
 import {Actions, Effect, ofType} from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {of} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
 export interface AuthResponseData {
   idToken: string;
@@ -34,20 +35,58 @@ export class AuthEffects {
             const expirationDate = new Date(
               new Date().getTime() + +resData.expiresIn * 1000
             );
-            return of(new AuthActions.Login({
+            return new AuthActions.Login({
               email: resData.email,
               userId: resData.localId,
               token: resData.idToken,
               expirationDate
-            }));
+            });
           }),
-          catchError(error => {
-            return of();
+          catchError(errorResponse => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorResponse.error || !errorResponse.error.error) {
+              return of(new AuthActions.LoginFail(errorMessage));
+            }
+            switch (errorResponse.error.error.message) {
+              case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account.';
+                break;
+              case 'OPERATION_NOT_ALLOWED':
+                errorMessage = 'Password sign-in is disabled for this project';
+                break;
+              case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+              case 'EMAIL_NOT_FOUND':
+                errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+              case 'INVALID_PASSWORD':
+                errorMessage = 'The password is invalid or the user does not have a password.';
+                break;
+              case 'USER_DISABLED':
+                errorMessage = 'The user account has been disabled by an administrator.';
+                break;
+            }
+            return of(new AuthActions.LoginFail(errorMessage));
           })
         );
     })
   );
 
-  constructor(private actions$: Actions, private httpClient: HttpClient) {
+  @Effect({
+    dispatch: false
+  })
+  authSuccess = this.actions$.pipe(
+    ofType(AuthActions.LOGIN),
+    tap(() => {
+      this.router.navigate(['/']).then();
+    })
+  );
+
+  constructor(
+    private actions$: Actions,
+    private httpClient: HttpClient,
+    private router: Router
+  ) {
   }
 }
